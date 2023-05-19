d8851e54ea5206709e858351138b61fa14f24ed5
liufei <l1ufei@163.com>
2023-05-19 11:49:29 +0800

    feat: NodeWrap添加数据监听 & 渲染判断

--- src/components/Workflow/modules/NodeWrap.vue
+++ src/components/Workflow/modules/NodeWrap.vue
@@ -1,28 +1,64 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { NodeType } from "../type";
 import PersonNode from "./nodes/PersonNode.vue";
 import AddNode from "./nodes/AddNode.vue";
+import ConditionNode from "./nodes/ConditionNode.vue";
 import { WorkflowNodeTypeEnum } from "../constants";
 
 const props = defineProps<{
 		modelValue: NodeType;
 	}>(),
+	emits = defineEmits<{
+		(e: "update:modelValue", modelValue: NodeType): void;
+	}>(),
 	/** 保存节点数据 */
 	nodeData = ref<NodeType>(props.modelValue);
 
-console.log("WorkflowNodeTypeEnum", WorkflowNodeTypeEnum.Approver);
+watch(
+	() => props.modelValue,
+	val => {
+		nodeData.value = val;
+	},
+	{
+		deep: true
+	}
+);
+
+watch(
+	() => nodeData.value,
+	val => {
+		emits("update:modelValue", val);
+	},
+	{
+		deep: true
+	}
+);
 </script>
 
 <template>
 	<div class="node-wrap">
-		<PersonNode v-model="nodeData"></PersonNode>
-		<AddNode v-model:node-children="nodeData.children"></AddNode>
+		<PersonNode
+			v-if="
+				props.modelValue.type === WorkflowNodeTypeEnum.Initiator ||
+				props.modelValue.type === WorkflowNodeTypeEnum.Approver ||
+				props.modelValue.type === WorkflowNodeTypeEnum.Copy
+			"
+			v-model="nodeData"
+		></PersonNode>
+		<ConditionNode v-else-if="props.modelValue.type === WorkflowNodeTypeEnum.Conditions" v-model="nodeData"></ConditionNode>
+		<AddNode v-if="nodeData" v-model:node-children="nodeData.children"></AddNode>
 	</div>
 	<!-- NodeWrap 递归渲染 -->
-	<NodeWrap v-if="nodeData.children" v-model="nodeData.children"></NodeWrap>
+	<NodeWrap v-if="nodeData?.children" v-model="nodeData.children"></NodeWrap>
 </template>
 
 <style scoped lang="scss">
 @import "../variable.scss";
+.node-wrap {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	min-width: 100%;
+}
 </style>
